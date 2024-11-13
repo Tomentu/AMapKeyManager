81f955b6d86086f7eae28b59c119b736cf6221be
何欢 <hehuan100@tastien.com>
2024-11-13 15:05:53 +0800

    update 更新类型

--- push_task.py
+++ push_task.py
@@ -19,8 +19,8 @@ import requests
 
 if __name__ == '__main__':
     # 读取Excel文件
-    start_rank = 59
-    end_rank = 68
+    start_rank = 96
+    end_rank = 100
     df = pd.read_excel('D:\城市-商圈排名信息.xlsx')
     #筛选rank大于start_rank小于end_rank的
     df = df[(df['rank'] >= start_rank)]
